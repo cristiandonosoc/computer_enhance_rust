7c799e1cbe1c6684c39265b3c65654c1d339f402
Cristián Donoso <cristiandonosoc@gmail.com>
2024-11-03 21:19:35 -0500

    Random point_generator
    Utility program that can generate arbitrary amount of points into json

--- src/bin/point_generator.rs
+++ src/bin/point_generator.rs
@@ -1,46 +1,49 @@
+use rand::Rng;
 use serde::Serialize;
 use std::fs::File;
 use std::io::Write;
-use rand::Rng;
+use std::error::Error;
 
 #[derive(Serialize)]
-struct Coord
-{
-    x: f64,
-    y: f64,
+struct Coord {
+    x0: f64,
+    y0: f64,
+    x1: f64,
+    y1: f64,
 }
-
-#[derive(Serialize)]
-struct CoordPair(Coord, Coord);
 
 fn generate_coord() -> Coord {
     let mut rng = rand::thread_rng();
 
     Coord {
-        x: rng.gen_range(-180.0..180.0),
-        y: rng.gen_range(-90.0..90.0),
+        x0: rng.gen_range(-180.0..180.0),
+        y0: rng.gen_range(-90.0..90.0),
+        x1: rng.gen_range(-180.0..180.0),
+        y1: rng.gen_range(-90.0..90.0),
     }
 }
 
-fn main() {
-    let args : Vec<String> = std::env::args().skip(1).collect();
-    if args.is_empty() {
-        panic!("Usage: point_generator <FILE>");
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.len() != 2 {
+        panic!("Usage: point_generator <COUNT> <FILE>");
     }
 
-    let num_pairs = 10;
-    let mut coords = Vec::with_capacity(num_pairs);
+    let count : usize = args[0].parse()?;
+    let filename = &args[1];
 
-    for _ in 0..num_pairs {
-        coords.push(CoordPair(generate_coord(), generate_coord()));
+    let mut coords = Vec::with_capacity(count);
+
+    for _ in 0..count{
+        coords.push(generate_coord());
     }
 
     // Convert to JSON.
-    let json = serde_json::to_string_pretty(&coords).unwrap();
+    let json = serde_json::to_string_pretty(&coords)?;
 
     // Write to file.
-    let mut file = File::create(&args[0]).unwrap();
-    file.write_all(json.as_bytes()).unwrap();
+    let mut file = File::create(filename)?;
+    file.write_all(json.as_bytes())?;
+
+    Ok(())
 }
-
-
