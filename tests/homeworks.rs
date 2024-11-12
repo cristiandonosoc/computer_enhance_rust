2a23c580ac4f63b1b33c37e215f086c9d57a4041
Cristián Donoso <cristiandonosoc@gmail.com>
2024-11-11 21:53:43 -0500

    [test] Reorganized tests into homework functions

---
+++ tests/homeworks.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+
+mod common;
+
+#[test]
+fn homework1() {
+    let listings = [
+        "listing_37.asm",
+        "listing_38.asm",
+    ];
+
+    for listing in listings {
+        if let Err(e) = common::run_nasm_test(listing) {
+            assert!(false, "{}", e);
+        }
+    }
+}
