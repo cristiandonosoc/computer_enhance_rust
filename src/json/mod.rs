f123e8d9bbe7792a189bb877fe76022614f19575
Cristián Donoso <cristiandonosoc@gmail.com>
2024-12-08 13:10:12 -0500

    [json] String and number parsing
    I got a bit of corners with the numbers, relying on the double parsing.
    But we could go to a paper if needed.

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -16,7 +16,7 @@ pub struct Object<'a> {
 }
 
 impl<'a> Object<'a> {
-    pub fn get<'b>(&'a self, want: &'b str) -> Option<&'a JsonValue<'a>> {
+    pub fn get(&'a self, want: &str) -> Option<&'a JsonValue<'a>> {
         // Go over the keys.
         for (key, value) in &self.pairs {
             if *key == want {
@@ -25,6 +25,15 @@ impl<'a> Object<'a> {
         }
 
         None
+    }
+
+    pub fn keys(&self) -> Vec<&str> {
+        let mut keys = Vec::with_capacity(self.pairs.len());
+        for (key, _) in &self.pairs {
+            keys.push(*key);
+        }
+
+        keys
     }
 }
 
@@ -47,7 +56,7 @@ pub fn parse(bytes: &[u8]) -> Result<JsonValue, Error> {
     Ok(output.value)
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Token<'a> {
     Invalid,
     EOF,
@@ -58,7 +67,6 @@ pub enum Token<'a> {
     Colon,        // :
     Comma,        // ,
     String(&'a str),
-    Integer,
     Number(f64),
     Bool(bool),
     Null,
@@ -180,12 +188,272 @@ fn parse_object(bytes: &[u8], mut index: usize) -> Result<ParseOutput, Error> {
     })
 }
 
-fn parse_token(bytes: &[u8], index: usize) -> Result<(Token, usize), Error> {
-    todo!("Parse stuff")
+fn parse_token(bytes: &[u8], mut index: usize) -> Result<(Token, usize), Error> {
+    if index >= bytes.len() {
+        return Ok((Token::EOF, bytes.len()));
+    }
+
+    loop {
+        let curr = bytes[index];
+        index += 1;
+        match curr {
+            // Skip whitespace.
+            b' ' | b'\r' | b'\t' | b'\n' => continue,
+            b'[' => return Ok((Token::LeftBracket, index)),
+            b']' => return Ok((Token::RightBracket, index)),
+            b'{' => return Ok((Token::LeftBrace, index)),
+            b'}' => return Ok((Token::RightBrace, index)),
+            b',' => return Ok((Token::Comma, index)),
+            b'"' => {
+                return parse_string(bytes, index);
+            }
+            b't' => {
+                if let Some(index) = seek(bytes, index, b"rue") {
+                    return Ok((Token::Bool(true), index));
+                }
+                return Err(Error::new(InvalidData, "Expected \"true\""));
+            }
+            b'f' => {
+                if let Some(index) = seek(bytes, index, b"alse") {
+                    return Ok((Token::Bool(false), index));
+                }
+                return Err(Error::new(InvalidData, "Expected \"false\""));
+            }
+            b'n' => {
+                if let Some(index) = seek(bytes, index, b"ull") {
+                    return Ok((Token::Null, index));
+                }
+                return Err(Error::new(InvalidData, "Expected \"null\""));
+            }
+            b'-' | b'0'..=b'9' => {
+                // We reparse the first number.
+                return parse_number(bytes, index - 1);
+            }
+            b':' => return Ok((Token::Colon, index)),
+            _ => return Err(Error::new(InvalidData, "Unexpected character")),
+        }
+    }
 }
 
-const LEFT_BRACKET: u8 = b'[';
-const RIGHT_BRACKET: u8 = b']';
-const LEFT_BRACE: u8 = b'{';
-const RIGHT_BRACE: u8 = b'}';
-const COMMA: u8 = b',';
+fn seek(bytes: &[u8], mut index: usize, pattern: &[u8]) -> Option<usize> {
+    if index + pattern.len() >= bytes.len() {
+        return None;
+    }
+
+    // Go one by one in the characters searching for mismatches.
+    for b in pattern {
+        if bytes[index] != *b {
+            return None;
+        }
+
+        index += 1
+    }
+
+    return Some(index);
+}
+
+// IMPORTANT: The index already considers parsing the initial double quote (one past it).
+fn parse_string(bytes: &[u8], mut index: usize) -> Result<(Token, usize), Error> {
+    let start_index: usize = index;
+    let mut curr: u8 = bytes[index];
+
+    // TODO: Support escaping.
+    let mut closed = false;
+    loop {
+        match curr {
+            b'"' => {
+                closed = true;
+                break;
+            }
+            _ => index += 1,
+        }
+
+        if index >= bytes.len() {
+            break;
+        }
+
+        curr = bytes[index];
+    }
+
+    if !closed {
+        return Err(Error::new(InvalidData, "Unclosed string"));
+    }
+
+    let string: &str;
+    unsafe {
+        string = std::str::from_utf8_unchecked(&bytes[start_index..index]);
+    }
+
+    // We return the index *after* the last double quote.
+    Ok((Token::String(string), index + 1))
+}
+
+fn parse_number(bytes: &[u8], mut index: usize) -> Result<(Token, usize), Error> {
+    // TODO: Maybe implement https://arxiv.org/abs/2101.11408 if we want to be mega fast?
+
+    let start_index: usize = index;
+
+    let mut curr: u8 = bytes[index];
+
+    loop {
+        match curr {
+            b'-' | b'+' | b'e' | b'E' | b'.' | b'0'..=b'9' => index += 1,
+            _ => break,
+        }
+
+        if index >= bytes.len() {
+            break;
+        }
+
+        curr = bytes[index];
+    }
+
+    let string: &str;
+    unsafe {
+        string = std::str::from_utf8_unchecked(&bytes[start_index..index]);
+    }
+    let num: f64 = string
+        .parse()
+        .map_err(|e| Error::new(InvalidData, format!("Parsing float \"{}\": {}", string, e)))?;
+
+    Ok((Token::Number(num), index))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod parse_number {
+        use super::*;
+
+        #[test]
+        fn test_parse_basic_integers() {
+            let bytes = b"123";
+            let (token, index) = parse_number(bytes, 0).unwrap();
+            assert_eq!(token, Token::Number(123.0));
+            assert_eq!(index, 3);
+        }
+
+        #[test]
+        fn test_parse_decimal() {
+            let bytes = b"123.456";
+            let (token, index) = parse_number(bytes, 0).unwrap();
+            assert_eq!(token, Token::Number(123.456));
+            assert_eq!(index, 7);
+        }
+
+        #[test]
+        fn test_parse_negative() {
+            let bytes = b"-123.456";
+            let (token, index) = parse_number(bytes, 0).unwrap();
+            assert_eq!(token, Token::Number(-123.456));
+            assert_eq!(index, 8);
+        }
+
+        #[test]
+        fn test_parse_scientific() {
+            let bytes = b"1.23e2";
+            let (token, index) = parse_number(bytes, 0).unwrap();
+            assert_eq!(token, Token::Number(123.0));
+            assert_eq!(index, 6);
+        }
+
+        #[test]
+        fn test_parse_error_invalid() {
+            let bytes = b"12.34.56";
+            assert!(parse_number(bytes, 0).is_err());
+        }
+
+        #[test]
+        fn test_parse_at_offset() {
+            let bytes = b"abc123.456   ";
+            let (token, index) = parse_number(bytes, 3).unwrap();
+            assert_eq!(token, Token::Number(123.456));
+            assert_eq!(index, 10);
+        }
+    }
+
+    mod parse_string {
+        use super::*;
+
+        mod string_parsing {
+            use super::*;
+
+            #[test]
+            fn test_basic_string() {
+                let bytes = b"\"hello\"";
+                let (token, index) = parse_string(bytes, 1).unwrap();
+                assert_eq!(token, Token::String("hello"));
+                assert_eq!(index, 7);
+            }
+
+            #[test]
+            fn test_empty_string() {
+                let bytes = b"\"\"";
+                let (token, index) = parse_string(bytes, 1).unwrap();
+                assert_eq!(token, Token::String(""));
+                assert_eq!(index, 2);
+            }
+
+            #[test]
+            fn test_string_with_spaces() {
+                let bytes = b"\"hello world\"";
+                let (token, index) = parse_string(bytes, 1).unwrap();
+                assert_eq!(token, Token::String("hello world"));
+                assert_eq!(index, 13);
+            }
+
+            #[test]
+            fn test_string_at_offset() {
+                let bytes = b"abc\"hello\"";
+                let (token, index) = parse_string(bytes, 4).unwrap();
+                assert_eq!(token, Token::String("hello"));
+                assert_eq!(index, 10);
+            }
+
+            #[test]
+            fn test_unclosed_string() {
+                let bytes = b"\"hello";
+                assert!(matches!(
+                    parse_string(bytes, 1),
+                    Err(e) if e.kind() == InvalidData
+                ));
+            }
+
+            #[test]
+            fn test_empty_input() {
+                // This case is special since our code assumes we already processed the first ".
+                let bytes = b"\"";
+                let (token, index) = parse_string(bytes, 0).unwrap();
+                assert_eq!(token, Token::String(""));
+                assert_eq!(index, 1);
+            }
+        }
+    }
+
+    //mod cases {
+    //    use super::*;
+
+    //    #[test]
+    //    fn test_simple_case() {
+    //        let bytes = b"
+    //        {
+    //            \"pairs\":[
+    //                {
+    //                    \"X0\":59.80934677533466,
+    //                    \"Y0\":-66.7913443851547,
+    //                    \"X1\":80.60590027144059,
+    //                    \"Y1\":-37.56716479818089
+    //                },
+    //            ]
+    //        }";
+
+    //        let value = parse(bytes).unwrap();
+    //        let JsonValue::Object(object) = value else {
+    //            panic!("Expected object");
+    //        };
+
+    //        assert_eq!(object.keys(), vec!["pairs"]);
+    //    }
+    //}
+}
