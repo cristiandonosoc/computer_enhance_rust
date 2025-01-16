e8afd08624b6f84a3d97a397c703ed5a60aa87fe
Cristián Donoso <cristiandonosoc@gmail.com>
2025-01-16 17:39:23 -0300

    [cpp] Read RDTSC and QueryPerformanceCounter
    Basic building blocks

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <intrin.h>
+#include <Windows.h>
+
+using u64 = uint64_t;
+
+u64 ReadCPUTimer() {
+	return __rdtsc();
+}
+
+u64 ReadOSTimer() {
+	LARGE_INTEGER counter;
+	QueryPerformanceCounter(&counter);
+	return counter.QuadPart;
+}
 
 void main() {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "RDTSC: " << ReadCPUTimer() << std::endl;
+	std::cout << "QueryPerformanceCounter: " << ReadOSTimer() << std::endl;
 }
 
