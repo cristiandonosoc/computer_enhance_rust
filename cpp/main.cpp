e7502b53e66ed104e5bff0e681cf12b3e1bbe587
Cristián Donoso <cristiandonosoc@gmail.com>
2025-01-16 18:18:12 -0300

    [cpp] Basic C++20 support
    Also added basic clang-format, clangd support.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,21 +1,76 @@
-#include <iostream>
-#include <intrin.h>
 #include <Windows.h>
+#include <format>
+#include <intrin.h>
+#include <iostream>
+#include <profileapi.h>
 
 using u64 = uint64_t;
+using f64 = double;
 
-u64 ReadCPUTimer() {
-	return __rdtsc();
-}
+u64 ReadCPUTimer() { return __rdtsc(); }
 
 u64 ReadOSTimer() {
-	LARGE_INTEGER counter;
-	QueryPerformanceCounter(&counter);
-	return counter.QuadPart;
+  LARGE_INTEGER counter;
+  QueryPerformanceCounter(&counter);
+  return counter.QuadPart;
 }
 
-void main() {
-	std::cout << "RDTSC: " << ReadCPUTimer() << std::endl;
-	std::cout << "QueryPerformanceCounter: " << ReadOSTimer() << std::endl;
+u64 ReadOSFreq() {
+  LARGE_INTEGER freq;
+  QueryPerformanceFrequency(&freq);
+  return freq.QuadPart;
 }
 
+u64 EstimateCPUFrequency(u64 wait_time_ms = 100) {
+  u64 cpu_start = ReadCPUTimer();
+  u64 os_start = ReadOSTimer();
+
+  u64 os_end = 0;
+  u64 os_elapsed = 0;
+
+  u64 os_freq = ReadOSFreq();
+  u64 os_wait_time = os_freq * wait_time_ms / 1000;
+  while (os_elapsed < os_wait_time) {
+    os_end = ReadOSTimer();
+    os_elapsed = os_end - os_start;
+  }
+
+  u64 cpu_end = ReadCPUTimer();
+  u64 cpu_elapsed = cpu_end - cpu_start;
+
+  u64 cpu_freq = 0;
+  if (os_elapsed > 0) {
+    cpu_freq = os_freq * cpu_elapsed / os_elapsed;
+  }
+
+  return cpu_freq;
+}
+
+std::string PrintFreq(u64 frequency) {
+  f64 freq = static_cast<f64>(frequency);
+  if (freq < 1000.0) {
+    return std::format("{:.4} Hz", freq);
+  }
+
+  freq = freq / 1000.0;
+  if (freq < 1000.0) {
+    return std::format("{:.4} KHz", freq);
+  }
+
+  freq = freq / 1000.0;
+  if (freq < 1000.0) {
+    return std::format("{:.4} MHz", freq);
+  }
+
+  freq = freq / 1000.0;
+  return std::format("{:.4} GHz", freq);
+}
+
+int main() {
+  std::cout << "RDTSC: " << ReadCPUTimer() << std::endl;
+  std::cout << "QueryPerformanceCounter: " << ReadOSTimer() << std::endl;
+
+  u64 freq = EstimateCPUFrequency();
+  std::cout << "Estimated CPU Frequency: " << freq << ", " << PrintFreq(freq)
+            << std::endl;
+}
