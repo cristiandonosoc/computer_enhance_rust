cd370713400107d9feec85d9971c3d717495f158
Cristián Donoso <cristiandonosoc@gmail.com>
2025-01-17 15:23:14 -0300

    [cpp] Add forward/backward repetition testing

--- cpp/repetition_test.cpp
+++ cpp/repetition_test.cpp
@@ -1,35 +1,64 @@
 #include "repetition_test.inl"
-#include "perf.inl"
 
 #include <assert.h>
 #include <memoryapi.h>
 #include <windows.h>
 
+#include "perf.inl"
+
 constexpr u64 kSize = GIGABYTE;
 
+u8* gBuffer = nullptr;
+
+void AddWriteRepetitionTest(const char* name, bool alloc_everytime,
+                            bool forward) {
+    auto test = [alloc_everytime, forward](TestRun* run) {
+        u8* buffer = gBuffer;
+        if (alloc_everytime) {
+            buffer = static_cast<u8*>(VirtualAlloc(
+                NULL, kSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
+        }
+        assert(buffer);
+
+        run->Bytes = kSize;
+        run->StartCycles = ReadCPUTimer();
+        run->StartPageFaults = ReadPageFaults();
+
+        // Write all of it.
+        if (forward) {
+            for (u64 i = 0; i < kSize; i++) {
+                buffer[i] = (u8)i;
+            }
+        } else {
+            for (i64 i = kSize - 1; i >= 0; i--) {
+                buffer[i] = (u8)i;
+            }
+        }
+
+        run->EndCycles = ReadCPUTimer();
+        run->EndPageFaults = ReadPageFaults();
+
+        if (alloc_everytime) {
+            VirtualFree(buffer, 0, MEM_RELEASE);
+        }
+    };
+
+    AddRepetitionTest(name, std::move(test));
+}
+
 int main() {
-  InitProcessData();
+    InitProcessData();
 
-  AddRepetitionTest("WriteAllBytesForward (alloc everytime)", [](TestRun *run) {
-    u8 *buffer = (u8 *)VirtualAlloc(NULL, kSize, MEM_COMMIT | MEM_RESERVE,
-                                    PAGE_READWRITE);
-    assert(buffer);
+    gBuffer = (u8*)VirtualAlloc(NULL, kSize, MEM_COMMIT | MEM_RESERVE,
+                                PAGE_READWRITE);
 
-    run->Bytes = kSize;
-    run->StartCycles = ReadCPUTimer();
-    run->StartPageFaults = ReadPageFaults();
+    AddWriteRepetitionTest("Forward (alloc everytime)", true, true);
+    AddWriteRepetitionTest("Backward (alloc everytime)", true, false);
 
-    // Write all of it.
+    AddWriteRepetitionTest("Forward (alloc once)", false, true);
+    AddWriteRepetitionTest("Backward (alloc once)", false, false);
 
-    for (u64 i = 0; i < kSize; i++) {
-      buffer[i] = (u8)i;
-    }
+    RunRepetitionTests(10);
 
-    run->EndCycles = ReadCPUTimer();
-    run->EndPageFaults = ReadPageFaults();
-
-    VirtualFree(buffer, 0, MEM_RELEASE);
-  });
-
-  RunRepetitionTests(10);
+    VirtualFree(gBuffer, 0, MEM_RELEASE);
 }
